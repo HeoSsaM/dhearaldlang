d607cdb6b7783811709585968e8f9a6880e4522c
허혜정 <blessme.5004@gmail.com>
2025-07-13 00:40:29 +0900

    디헤럴드플립 메인페이지

---
+++ js/swiper.js
@@ -0,0 +1,34 @@
+//index 
+var swiper = new Swiper(".mainHero", {
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//파트너사
+const swiperSlides = document.querySelectorAll('.partnerBanner');
+
+swiperSlides.forEach(function (element, index) {
+    element.classList.add("swiper-" + index);
+    let swiper = new Swiper(".swiper-" + index, {
+      autoplay: {
+            delay: 1,
+            desableOnInteraction: false,
+      },
+      speed: 8e3,
+      loop: true,
+      slidesPerView: "auto",
+      freemode: true,      
+    });
+});
